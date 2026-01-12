1e6733f65836719c660b87de5edd3ab292690a7a
Kanta Hirazawa <s24C1101LD@s.chibakoudai.jp>
2026-01-12 21:59:44 +0900

    edit

--- test/test.bash
+++ test/test.bash
@@ -7,12 +7,22 @@ dir=~
 
 cd $dir/ros2_ws
 colcon build
-source install/setup.bash
+source $dir/ros2_ws/install/setup.bash
 
-export PYTHONUNBUFFERED=1
+ros2 launch mypkg talk_listen.launch.py > /dev/null 2>&1 &
+PID=$!
 
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log 2>&1
+sleep 5
 
-cat /tmp/mypkg.log
+timeout 15 ros2 topic echo /load_avg > /tmp/mypkg_log.txt
 
-grep 'Current CPU Load' /tmp/mypkg.log
+kill $PID
+
+if grep -q "data:" /tmp/mypkg_log.txt; then
+    echo "Test Passed: Topic /load_avg is active!"
+    exit 0
+else
+    echo "Test Failed: Topic /load_avg not received."
+    cat /tmp/mypkg_log.txt
+    exit 1
+fi
