f2ba468ebab438079e3085e7ba95a79fabfddff0
Kanta Hirazawa <s24C1101LD@s.chibakoudai.jp>
2026-01-12 22:50:29 +0900

    edit

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -6,7 +6,7 @@ import launch_ros.actions
 
 def generate_launch_description():
 
-    talker = launch_ros.actions.Node(
+    cpu_load = launch_ros.actions.Node(
             package='mypkg',
             executable='cpu_load',
             )
