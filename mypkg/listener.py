11a479763dbd5d9c7d1c1fb12d9b5eecdff95dd3
Kanta Hirazawa <s24C1101LD@s.chibakoudai.jp>
2026-01-12 19:51:01 +0900

    edit

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -5,14 +5,26 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
-rclpy.init()
-node = Node("listener")
+class Listener(Node):
+    def __init__(self):
+        super().__init__('listener')
+        self.create_subscription(Int16, 'battery', self.cb, 10)
 
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %d" % msg.data)
-
+    def cb(self, msg):
+        self.get_logger().info(f'Received Battery Level: {msg.data}%')
+        
+        if msg.data > 0 and msg.data < 20:
+            self.get_logger().warn('Battery is Low! Please charge.')
 
 def main():
-    pub = node.create_subscription(Int16, "countup", cb, 10)
-    rclpy.spin(node)
+    rclpy.init()
+    node = Listener()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
